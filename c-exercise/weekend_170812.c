c5c960b24718631903907278f0f6f4f821b091db
Youk Chang SU <csyouk@gmail.com>
2017-08-12 17:24:15 +0900

    update

--- c-exercise/weekend_170812.c
+++ c-exercise/weekend_170812.c
@@ -1,23 +1,37 @@
 #include <stdio.h>
 
-#if 1
+#if 0
+int fact(int n)
+{
+	if (n == 0) return 1;
+	return n*fact(n - 1);
+}
+void main(void)
+{
+	printf("%d\n", fact(5));
+}
+#endif
+
+
+#if 0
 int prt(int x)
 {
 	if (x == 0)return x;
 	else if (x % 2)
 	{
-		printf("1 - %d\n", x + prt(x - 1));
+		printf("%d\n", x + prt(x - 1));
 		return x;
 	}
 	else
 	{
-		printf("2 - %d\n", x + prt(x - 1));
+		printf("%d\n", x + prt(x - 1));
 		return x;
 	}
 }
 void main(void)
 {
 	int i;
-	printf("3 - %d\n", i = prt(5));
+	prt(5);
+	//printf("3 - %d\n", i = prt(5));
 }
 #endif
